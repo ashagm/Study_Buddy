5215ab6e662974a38ad79154a56c52084e469c19
asha <ashaagm@gmail.com>
2018-03-14 09:13:57 -0400

    including hbs files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,32 +1,13 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+module.exports = function(app){
+	app.get('/', function(req, res) {
+    	res.redirect('/signin');
+	});
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+	app.get('/signup', function(req,res){
+		res.render('signup');
+	});
 
-module.exports = function(app) {
-
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    console.log(req);
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname + "/../public/signup.html"));
-  });
-
-  app.get("/signin", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname + "/../public/signin.html"));
-  });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/members.html"));
-  });
-
-};
+	app.get('/signin', function(req,res){
+		res.render('signin');
+	});
+}
