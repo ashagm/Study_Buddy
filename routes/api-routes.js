44f872a2c706a30dff71c71a702e96ca9ef4ea3a
asha <ashaagm@gmail.com>
2018-03-16 08:06:54 -0400

    initial working of sessions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,91 +6,69 @@ const saltRounds = 10;
 
 module.exports = function(app) {
 
-app.get("/api/all", function(req, res) {
-    db.user.findAll({}).then(function(results) {
-      res.json(results);
+    app.get("/api/all", function(req, res) {
+        db.user.findAll({}).then(function(results) {
+          res.json(results);
+        });
     });
-});
 
-app.post("/api/signup", function(req, res) {
-    const newUser = req.body;
-    // console.log("newUser",newUser);
+    app.post("/api/signup", function(req, res) {
+        const newUser = req.body;
 
-    bcrypt.hash(newUser.password, saltRounds, function(err, hash) {
-        newUser.password = hash;
+        bcrypt.hash(newUser.password, saltRounds, function(err, hash) {
+            newUser.password = hash;
 
-        db.user.create({
-          first_name: newUser.firstName,
-          last_name: newUser.lastName,
-          user_email: newUser.email,
-          user_name: newUser.userName,
-          user_password: newUser.password
-        }).then(function(result){
-            // console.log(result);
-            // res.render('signin');
-            // res.json({id: result.insertId});
-        });              
-     });
+            db.user.create({
+              first_name: newUser.firstName,
+              last_name: newUser.lastName,
+              user_email: newUser.email,
+              user_name: newUser.userName,
+              user_password: newUser.password
+            }).then(function(result){
+                console.log(result);
+                res.redirect(307, "/signin");
+            }).catch(function(err) {
+                console.log(err);
+                res.json(err);
+            });                
+         });
 
     });  
 
-app.post("/api/signin", function(req, res) {
-    const loginUser = req.body;
+    app.post("/api/signin", function(req, res) {
+        const loginUser = req.body;
 
-    let reqPassword = req.body.password;
-    let reqUserName = req.body.userName;
+        let reqPassword = req.body.password;
+        let reqUserName = req.body.userName;
 
-    db.user.findOne({
-        where: {
-            user_name: reqUserName
-        }
-    }).then(function(dbUser) {
-        // console.log("dbUser", dbUser);
-        res.json(dbUser);
+        db.user.findOne({
+            where: {
+                user_name: reqUserName
+            }
+        }).then(function(dbUser) {
+            console.log("dbUser", dbUser.user_password);
+            console.log("reqPassword", reqPassword);
+            // res.json(dbUser);
 
-        bcrypt.compare(reqPassword, dbUser.user_password, function(err, result) {
-            if(result) {
-                console.log("Passwords match! You are logged in");
-                
-                // sets a cookie with the user's info
-                req.session.user = dbUser;
-                //redirect to right place //todo
-                
-                console.log("Session", req.session);
-                console.log("Session user", req.session.user);
+            bcrypt.compare(reqPassword, dbUser.user_password, function(err, result) {
+                console.log("bcrypt response", result);
+                if(result) {
+                    console.log("passwords match");
+                    // sets a cookie with the user's info
+                    // console.log(req.mySession);
+                    req.mySession.user = dbUser;
+                    // console.log(req.mySession.user);
+                    res.redirect('/dashboard');
+                } else {
+                    console.log("passwords dont match");
+                    //redirect to right place //todo
+                    res.json(err);
+                } 
+            });
 
-            } else {
-                console.log("passwords dont match");
-                //redirect to right place //todo
-            } 
+        }).catch(function(err){
+            console.log(err);
         });
-    }).catch(function(err){
-        console.log(err);
-    });
-});  
+    });  
 
-app.get('/dashboard', function(req, res) {
-    console.log("you are in /dashboard");
-    console.log(req.session);
-    console.log(req.session.user);
-  if (req.session && req.session.user) { // Check if session exists
-    // lookup the user in the DB by pulling their email from the session
-    db.user.findOne({ user_name: req.session.user.user_name }, function (err, user) {
-      if (!user) {
-        // if the user isn't found in the DB, reset the session info and
-        // redirect the user to the login page
-        req.session.reset();
-        res.redirect('/signin');
-      } else {
-        // expose the user to the template
-        res.locals.user = user;
- 
-        // render the dashboard page
-        res.render('dashboard');
-      }
-    });
-  } else {
-    res.redirect('/signin');
-  }
-});
 }
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const db = require('./models');
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
+
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,13 +25,14 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //session handler middleware
-
-app.use(session({
-	cookieName: 'session',
-	secret: 'keep_this_a_secret',
-	duration: 30 * 60 * 1000,
-	activeDuration : 5 * 60 * 1000
-}));
+app.use(
+	session({
+  		cookieName: 'mySession', // cookie name dictates the key name added to the request object 
+  		secret: 'thisisasecretkept', // should be a large unguessable string 
+  		duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms 
+  		activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds 
+	})
+);
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
