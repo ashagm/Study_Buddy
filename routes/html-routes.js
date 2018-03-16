3f32af143bd6ed6e06290db63341dcacb3f3bcac
asha <ashaagm@gmail.com>
2018-03-16 09:59:22 -0400

    fixes rerouting to dashboard

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -23,34 +23,21 @@ module.exports = function(app){
 
 //this requires some more tweaks to include the session as a middleware 
 //function so that it is available in every path.
-//
+
 	app.get("/dashboard", function(req, res) {
 		console.log("Is there a session? ", req.mySession);
 
 		if (req.mySession && req.mySession.user) { // Check if session exists
-		    // lookup the user in the DB by pulling their username from the session
 		    let userName =  req.mySession.user.user_name;
-		    // res.render("dashboard", {"userName" : userName});
-		    db.user.findOne(
-		    	{ 
-		    		user_name: userName
-		    	}, function (err, user) {
-		    		console.log("found!", user);
-			      if (!user) {
-			        req.mySession.reset();
-			        res.redirect('/signin');
-			      } else {
-			        // expose the user to the template
-			        res.locals.user = user;			 
-			        // render the dashboard page
-			        res.render('dashboard', {"userName" : userName});
-			      }
-		    });
+		     console.log(userName);
+
+		    let loggedInUser = req.mySession.user; 
+	        res.locals.user = loggedInUser;			 
+	        res.render('dashboard', {"userId" : loggedInUser.id});
 	  	} else {
 		  	console.log("in else");
-		    // res.redirect('/signin');
+		    res.redirect('/signin');
 		  }
-		// res.render("dashboard");
 	});
 
 }
