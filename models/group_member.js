4250e296dbda349d5675e8e41a58f16e86503089
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-03-17 13:04:19 -0400

    user and group tables joined within group members table

--- models/group_member.js
+++ models/group_member.js
@@ -10,3 +10,26 @@ module.exports = (sequelize, DataTypes) => {
   };
   return group_member;
 };
+
+// -module.exports = function(sequelize, DataTypes) {
+// -	const Group_member = sequelize.define('group_member', {
+// -		group_id: 	{
+// -						type: DataTypes.INTEGER,
+// -          				allowNull: false,
+// -          				foreignKey: true
+// -					},
+// -		user_id: 	{ 
+// -						type: DataTypes.INTEGER, 
+// -						allowNull: false,
+// -						foreignKey: true, 
+// -						validate: { notEmpty: true}
+// -					},
+// -		is_admin: 	{ 
+// -						type: DataTypes.BOOLEAN, 
+// -						allowNull: false,
+// -						defaultValue: false
+// -					}
+// -
+// -  	});
+// -
+// -	return Group_member;
--- models/user.js
+++ models/user.js
@@ -13,3 +13,42 @@ module.exports = (sequelize, DataTypes) => {
   };
   return user;
 };
+
+// -var bcrypt = require('bcrypt');
+// -const saltRounds = 10;
+// -
+// -module.exports = function(sequelize, DataTypes) {
+// - const User = sequelize.define('user', {
+// -   first_name: { 
+// -           type: DataTypes.STRING, 
+// -           unique: false, 
+// -           allowNull: false, 
+// -           validate: { notEmpty: true}
+// -         },
+// -   last_name: { 
+// -           type: DataTypes.STRING, 
+// -           unique: false, 
+// -           allowNull: false, 
+// -           validate: { notEmpty: true}
+// -         },
+// -   user_email: { 
+// -           type: DataTypes.STRING, 
+// -           unique: true, 
+// -           allowNull: false, 
+// -           validate: { notEmpty: true}
+// -         },
+// -   user_name: { 
+// -           type: DataTypes.STRING, 
+// -           unique: true, 
+// -           allowNull: false, 
+// -           validate: { notEmpty: true}
+// -         },
+// -   user_password: { 
+// -           type: DataTypes.STRING, 
+// -           unique: false, 
+// -           allowNull: false, 
+// -           validate: { notEmpty: true}
+// -         }
+// - });
+// -
+// - return User;
