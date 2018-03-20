e996ef44828d279b1d70edeecb6c3d539d82bf99
Cole Santiago <colesantiago@Coles-MBP.fios-router.home>
2018-03-19 22:28:38 -0400

    added delete button to the group details page. things that still need to be fixed: join group is disapearing permently for every user, and need to redirect user after a group is deleted

--- seeders/20180315231605-demo-group.js
+++ seeders/20180315231605-demo-group.js
@@ -2,8 +2,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('groups', [{
-        group_name: 'splatoon',
-        group_desc: 'Study group for splatoon'
+        group_name: 'video games',
+        group_desc: 'Study group for video games'
       }], {});
     
   },
