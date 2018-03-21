7db6feb8f6adac44cccf0b7f494f379fef619901
asha <ashaagm@gmail.com>
2018-03-20 20:59:07 -0400

    your groups route

--- models/index.js
+++ models/index.js
@@ -9,9 +9,9 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config, {logging:console.log});
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config, {logging:console.log});
 }
 
 fs
